a57689c79e25e8e99555f064f348c6c669ffb5b6
prudywsh <antoineprudhomme5@gmail.com>
2016-12-18 19:02:05 +0100

    node js test : url param added

--- node.js/first_app/server.js
+++ node.js/first_app/server.js
@@ -1,11 +1,12 @@
 let http = require('http');
 let fs = require('fs');
+let url = require('url');
 
 let server = http.createServer();
 
 server.on('request', (request, response) => {
 
-    fs.readFile('index.html', (err, data) => {
+    fs.readFile('index.html', 'utf8', (err, data) => {
 
         if (err) {
             response.writeHead(404, {
@@ -13,9 +14,15 @@ server.on('request', (request, response) => {
             });
             response.end('This file does not exists');
         } else {
+            let query = url.parse(request.url, true).query;
+            let name = query.name === undefined ? 'dude' : query.name;
+
             response.writeHead(200, {
                 'Content-Type': 'text/html; charset=utf8'
             });
+
+            data = data.replace('{{ name }}', name);
+
             response.end(data);
         }
 
